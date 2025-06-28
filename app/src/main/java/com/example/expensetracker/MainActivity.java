8431a1be29875f9f8dc22719f861acacf7f74ec6
fdsanti <desantifederico@gmail.com>
2025-06-28 11:11:27 -0300

    Mucho progreso
    Google auth funciona, y tengo environmen de debug y release

--- app/src/main/java/com/example/expensetracker/MainActivity.java
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -1,29 +1,24 @@
 package com.example.expensetracker;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.FirebaseApp;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -46,11 +41,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.sql.Array;
+import com.example.expensetracker.AuthGuard;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.sql.Connection;
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -86,20 +82,18 @@ public class MainActivity extends AppCompatActivity implements CallBackItemTouch
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        // Seguridad extra: si no está autenticado, volver al login
-        if (mAuth.getCurrentUser() == null) {
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-            return;
-        }
-
-
-        setContentView(R.layout.activity_main2);
-        initializePage();
+        AuthGuard.checkAccess(this, () -> {
+            setContentView(R.layout.activity_main2);
+            initializePage();
+        });
 
     }
+
+    public void redirectToLogin() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
@@ -131,80 +125,98 @@ public class MainActivity extends AppCompatActivity implements CallBackItemTouch
         viewPager.setSwipeable(false);
     }
 
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        //Craete Alert dialog
-        MaterialAlertDialogBuilder dialog = new MaterialAlertDialogBuilder(this);
-        dialog.setCancelable(false);
-        dialog.setTitle("Agregar reporte");
-        dialog.setMessage("Por favor escriba un nombre para el reporte");
-        //Add input field to dialog
-        LayoutInflater inflater = this.getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.input1, null);
-        dialog.setView(dialogView);
-        dialog.setNegativeButton("Cancelar", null);
-        dialog.setPositiveButton("Crear", null);
+        int id = item.getItemId();
 
-        AlertDialog alertDialog = dialog.create();
-        alertDialog.show();
+        if (id == R.id.btnRefresh) {
+            // Crear diálogo para agregar reporte
+            MaterialAlertDialogBuilder dialog = new MaterialAlertDialogBuilder(this);
+            dialog.setCancelable(false);
+            dialog.setTitle("Agregar reporte");
+            dialog.setMessage("Por favor escriba un nombre para el reporte");
 
-        //Click en "Crear" btn
-        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onClick(View v) {
-                TextInputEditText editText = dialogView.findViewById(R.id.edit_text);
-                //si el nombre ya existe, entonces mostrar error state
-                if (HCardDB.containsDescription(editText.getText().toString())) {
+            LayoutInflater inflater = this.getLayoutInflater();
+            View dialogView = inflater.inflate(R.layout.input1, null);
+            dialog.setView(dialogView);
+            dialog.setNegativeButton("Cancelar", null);
+            dialog.setPositiveButton("Crear", null);
 
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onClick(View v) {
+                    TextInputEditText editText = dialogView.findViewById(R.id.edit_text);
                     TextInputLayout inputField = dialogView.findViewById(R.id.filledTextField);
-                    inputField.setErrorEnabled(true);
-                    inputField.setError("El nombre ya existe. Elija otro.");
-                    inputField.setErrorIconDrawable(R.drawable.ic_info);
-                }
-                //si el input field esta vacio, mostrar error state
-                else if (editText.getText().toString().trim().isEmpty()) {
 
-                    TextInputLayout inputField = dialogView.findViewById(R.id.filledTextField);
-                    inputField.setErrorEnabled(true);
-                    inputField.setError("Es necesario elegir un nombre.");
-                    inputField.setErrorIconDrawable(R.drawable.ic_info);
-                }
-                //Show progress bar and create new report
-                else {
-                    alertDialog.dismiss();
+                    String input = editText.getText().toString().trim();
+                    if (HCardDB.containsDescription(input)) {
+                        inputField.setErrorEnabled(true);
+                        inputField.setError("El nombre ya existe. Elija otro.");
+                        inputField.setErrorIconDrawable(R.drawable.ic_info);
+                    } else if (input.isEmpty()) {
+                        inputField.setErrorEnabled(true);
+                        inputField.setError("Es necesario elegir un nombre.");
+                        inputField.setErrorIconDrawable(R.drawable.ic_info);
+                    } else {
+                        alertDialog.dismiss();
+                        int newID = HCardDB.getBiggestID() + 1;
+                        HomeCard hc = new HomeCard(String.valueOf(newID), LocalDate.now(), input, false);
 
-                    //Find biggest Id and create the new report with id + 1
-                    int newID = HCardDB.getBiggestID() + 1;
-                    HomeCard hc = new HomeCard(String.valueOf(newID), LocalDate.now(), editText.getText().toString(),false);
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference();
 
-                    //add new card to Firebase DB & update the array
-                    FirebaseDatabase database = FirebaseDatabase.getInstance();
-                    DatabaseReference myRef = database.getReference();
-                    myRef.child("allTables").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DataSnapshot> task) {
-                            if (!task.isSuccessful()) {
-                                Log.e("firebase", "Error getting data", task.getException());
+                        myRef.child("allTables").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    myRef.child("allTables").child(hc.getTableID()).child("creationDate").setValue(String.valueOf(hc.getCreationDate()));
+                                    myRef.child("allTables").child(hc.getTableID()).child("tableDescription").setValue(hc.getName());
+                                    myRef.child("allTables").child(hc.getTableID()).child("tableName").setValue(hc.getTableID());
+                                    myRef.child("allTables").child(hc.getTableID()).child("cerrado").setValue(hc.isCerrado());
+
+                                    HCardDB.addExpense(String.valueOf(newID), hc);
+                                    actualFragment.addHCards(0, hc);
+                                    Toast.makeText(MainActivity.this, "¡El expense ha sido creado con éxito!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Log.e("firebase", "Error getting data", task.getException());
+                                }
                             }
-                            else {
-                                myRef.child("allTables").child(hc.getTableID()).child("creationDate").setValue(String.valueOf(hc.getCreationDate()));
-                                myRef.child("allTables").child(hc.getTableID()).child("tableDescription").setValue(hc.getName());
-                                myRef.child("allTables").child(hc.getTableID()).child("tableName").setValue(hc.getTableID());
-                                myRef.child("allTables").child(hc.getTableID()).child("cerrado").setValue(hc.isCerrado());
-                                HCardDB.addExpense(String.valueOf(newID), hc);
-                                actualFragment.addHCards(0,hc);
-                                Toast.makeText(MainActivity.this, "¡El expense ha sido creado con éxito!", Toast.LENGTH_SHORT).show();
-                                Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                            }
-                        }
-                    });
-
-
+                        });
+                    }
                 }
+            });
+
+            return true;
+
+        } else if (id == R.id.btnMore) {
+            View anchorView = findViewById(R.id.btnMore);
+            if (anchorView != null) {
+                PopupMenu popup = new PopupMenu(this, anchorView);
+                popup.getMenu().add("Cerrar sesión");
+
+                popup.setOnMenuItemClickListener(menuItem -> {
+                    if ("Cerrar sesión".contentEquals(menuItem.getTitle())) {
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(this, LoginActivity.class));
+                        finish();
+                        return true;
+                    }
+                    return false;
+                });
+
+                popup.show();
+
             }
-        });
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     public void itemTuchOnMove(int oldPosition, int newPosition) {
